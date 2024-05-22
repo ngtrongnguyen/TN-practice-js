d05678b58f3dff4a80d74f5eeef6f29c87e519c2
ngtrongnguyen <nguyentrongnguyen.011098@gmail.com>
2024-05-22 18:19:37 +0700

    remove file change

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
-import MovieController from './controller/movie.controller';
-import MovieService from './services/movie.service';
-import MovieView from './views/movie.views';
-new MovieController(new MovieService(), new MovieView());
-// new MovieView();
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(3000);
