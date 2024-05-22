0819df9853e31464f5f0139ad52b24effa7daa53
ngtrongnguyen <nguyentrongnguyen.011098@gmail.com>
2024-05-22 17:32:27 +0700

    remove file

--- src/helper/uid.js
+++
@@ -1,7 +0,0 @@
-function generateID(length = 4) {
-    return Math.random()
-      .toString(36)
-      .substring(2, length + 2);
-  }
-  
-  export default generateID;
--- src/views/components/navChild.js
+++
@@ -1,9 +0,0 @@
-const navChild = (array) => {
-  let listItems = '';
-  for (let i = 0; i < array.length; i++) {
-    listItems += `<li><a href="">${array[i]}</a></li>`;
-  }
-  return listItems;
-};
-
-export default navChild;
